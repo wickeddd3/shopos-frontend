953f6bffc854609f52894530632cdbf227b8fae2
wickeddd3 <pablophilipandrew@gmail.com>
2023-02-08 01:17:24 +0800

    reports page initial layout

---
+++ src/pages/Reports/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <app-content>
+    <h1>Reports</h1>
+  </app-content>
+</template>
+
+<script>
+import AppContent from '@/components/App/AppContent';
+
+export default {
+  name: 'Reports',
+  components: {
+    AppContent,
+  },
+};
+</script>
