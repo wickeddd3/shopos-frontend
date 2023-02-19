744b00b5955d90aad8599043d314f2c0f6184c55
wickeddd3 <pablophilipandrew@gmail.com>
2023-02-19 18:50:57 +0800

    add app dialog component on app content

--- src/components/App/AppContent.vue
+++ src/components/App/AppContent.vue
@@ -11,16 +11,25 @@
         <snackbar></snackbar>
         <breadcrumbs></breadcrumbs>
         <slot></slot>
+        <app-dialog
+          :show="dialogShow"
+          :component="dialogComponent"
+          :max-width="dialogMaxwidth"
+          @cancel="closeDialog"
+          @close="closeDialog"
+        ></app-dialog>
       </v-container>
     </v-main>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
 import Appbar from '@/components/Appbar/index.vue';
 import Sidebar from '@/components/Sidebar/index.vue';
 import Breadcrumbs from '@/components/Breadcrumbs';
 import Snackbar from '@/components/Snackbar';
+import AppDialog from '@/components/App/AppDialog';
 
 export default {
   name: 'AppContent',
@@ -29,6 +38,19 @@ export default {
     Sidebar,
     Breadcrumbs,
     Snackbar,
+    AppDialog,
+  },
+  computed: {
+    ...mapGetters({
+      dialogShow: 'appdialog/show',
+      dialogComponent: 'appdialog/component',
+      dialogMaxwidth: 'appdialog/maxwidth',
+    }),
+  },
+  methods: {
+    ...mapActions({
+      closeDialog: 'appdialog/close',
+    }),
   },
 };
 </script>
