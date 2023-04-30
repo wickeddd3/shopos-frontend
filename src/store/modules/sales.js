0611857002f762259b3cf66be3c2a223018fe616
wickeddd3 <pablophilipandrew@gmail.com>
2023-04-30 23:51:10 +0800

    add sales store

---
+++ src/store/modules/sales.js
@@ -0,0 +1,89 @@
+import initialState from '@/config/sales.state.js';
+import SalesResource from '@/api/sales/SalesResource';
+import moment from 'moment';
+
+const resource = new SalesResource();
+
+const state = {
+  ...initialState,
+};
+
+const getters = {
+  'yearly/ready': ({ yearly: { ready } }) => ready,
+  'yearly/data': ({ yearly: { data } }) => data,
+  'yearly/data/years': ({ yearly: { data } }) => (data || []).map(item => item.year),
+  'monthly/ready': ({ monthly: { ready } }) => ready,
+  'monthly/data': ({ monthly: { data } }) => data,
+  'monthly/data/months': ({ monthly: { data } }) => (data || []).map(item => item.month),
+  'monthly/data/months/formatted': ({ monthly: { data } }) => (data || []).map(item => ({
+    ...item,
+    month: moment(item.month).format('MMMM'),
+  })),
+  'monthly/labels': () => [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December',
+  ],
+  'monthly/total': (state, getters) => {
+    const monthlyData = getters['monthly/data/months/formatted'];
+    const montlyLabels = getters['monthly/labels'];
+    return montlyLabels.map(label => {
+      const data = monthlyData.find(item => item.month === label);
+      return (data ? data?.sales : 0) * 1;
+    });
+  },
+  'daily/ready': ({ daily: { ready } }) => ready,
+  'daily/data': ({ daily: { data } }) => data,
+  'daily/data/days': ({ daily: { data } }) => (data || []).map(item => item.date),
+  'daily/data/days/formatted': ({ daily: { data } }) => (data || []).map(item => ({
+    ...item,
+    date: moment(item.date).format('D')
+  })),
+  'daily/labels': () => [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+    '11', '12', '13', '14', '15', '16', '17', '18', '19',
+    '20', '21', '22', '23', '24', '25', '26', '27', '28',
+    '29', '30', '31',
+  ],
+  'daily/total': (state, getters) => {
+    const dailyData = getters['daily/data/days/formatted'];
+    const dailyLabels = getters['daily/labels'];
+    return dailyLabels.map(label => {
+      const data = dailyData.find(item => item.date === label);
+      return (data ? data?.sales : 0) * 1;
+    });
+  },
+};
+
+const mutations = {
+  'YEARLY/SET': (state, data) => { state.yearly = { ...state.yearly, ...data }; },
+  'MONTHLY/SET': (state, data) => { state.monthly = { ...state.monthly, ...data }; },
+  'DAILY/SET': (state, data) => { state.daily = { ...state.daily, ...data }; },
+};
+
+const actions = {
+  'yearly/get': async ({ commit }) => {
+    const { data } = await resource.yearly();
+    commit('YEARLY/SET', { data, ready: true });
+    return data;
+  },
+  'monthly/get': async ({ commit }, year) => {
+    const params = { year };
+    const { data } = await resource.monthly(params);
+    commit('MONTHLY/SET', { data, ready: true });
+    return data;
+  },
+  'daily/get': async ({ commit }, month) => {
+    const params = { month };
+    const { data } = await resource.daily(params);
+    commit('DAILY/SET', { data, ready: true });
+    return data;
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
